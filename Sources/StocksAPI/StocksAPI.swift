26ab5b93fb05fa337457e71dc883fcf011232342
Alberto Delle Donne <alberto.delledonne05@gmail.com>
2022-10-09 22:37:23 +0100

    Update StocksAPI.swift

--- Sources/StocksAPI/StocksAPI.swift
+++ Sources/StocksAPI/StocksAPI.swift
@@ -1,97 +1,124 @@
 import Foundation
 
-public struct StocksAPI {
-    private let session = URLSession.shared
-    private let jsonDecoder = {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .secondsSince1970
-        return decoder
-    }()
-    
-    private let baseURL = "https://query1.finance.yahoo.com"
+public protocol IStocksAPI {
+    func fetchChartData(tickerSymbol: String, range: ChartRange) async throws -> ChartData?
+    func fetchChartRawData(symbol: String, range: ChartRange) async throws -> (Data, URLResponse)
+    func searchTickers(query: String, isEquityTypeOnly: Bool) async throws -> [Ticker]
+    func searchTickersRawData(query: String, isEquityTypeOnly: Bool) async throws -> (Data, URLResponse)
+    func fetchQuotes(symbols: String) async throws -> [Quote]
+    func fetchQuotesRawData(symbols: String) async throws -> (Data, URLResponse)
+}
 
-    public init() {}
-    
-    public func fetchChartData(symbol: String, range: ChartRange) async throws -> ChartData? {
-        guard var urlComponents = URLComponents(string: "\(baseURL)/v8/finance/chart/\(symbol)") else {
-            throw APIError.invalidURL
+public struct StocksAPI: IStocksAPI {
+    private let session = URLSession.shared
+        private let jsonDecoder = {
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .secondsSince1970
+            return decoder
+        }()
+        
+        public init() {}
+        
+        private let baseURL = "https://query1.finance.yahoo.com"
+        public func fetchChartData(tickerSymbol: String, range: ChartRange) async throws -> ChartData? {
+            guard let url = urlForChartData(symbol: tickerSymbol, range: range) else { throw APIError.invalidURL }
+            let (resp, statusCode): (ChartResponse, Int) = try await fetch(url: url)
+            if let error = resp.error {
+                throw APIError.httpStatusCodeFailed(statusCode: statusCode, error: error)
+            }
+            return resp.data?.first
         }
         
-        urlComponents.queryItems = [
-            .init(name: "range", value: range.rawValue),
-            .init(name: "interval", value: range.interval),
-            .init(name: "indicators", value: "quote"),
-            .init(name: "includeTimestamps", value: "true")
-        ]
-        guard let url = urlComponents.url else {
-            throw APIError.invalidURL
-        }
-        let (response, statusCode): (ChartResponse, Int) = try await fetch(url: url)
-        if let error = response.error {
-            throw APIError.httpStatusCodeFailed(statusCode: statusCode, error: error)
-        }
-        return response.data?.first
-    }
-    
-    
-    public func searchTickers(query: String, isEquityTypeOnly: Bool = true) async throws -> [Ticker]{
-        guard var urlComponents = URLComponents(string: "\(baseURL)/v1/finance/search") else {
-            throw APIError.invalidURL
+        public func fetchChartRawData(symbol: String, range: ChartRange) async throws -> (Data, URLResponse) {
+            guard let url = urlForChartData(symbol: symbol, range: range) else { throw APIError.invalidURL }
+            return try await session.data(from: url)
         }
         
-        urlComponents.queryItems = [
-            .init(name: "q", value: query),
-            .init(name: "quotesCount", value: "20"),
-            .init(name: "lang", value: "en-US")
-        ]
-        guard let url = urlComponents.url else {
-            throw APIError.invalidURL
-        }
-        let (response, statusCode): (SearchTickersResponse, Int) = try await fetch(url: url)
-        if let error = response.error {
-            throw APIError.httpStatusCodeFailed(statusCode: statusCode, error: error)
-        }
-        if isEquityTypeOnly {
-            return (response.data ?? [])
-                .filter { ($0.quoteType ?? "").localizedCaseInsensitiveCompare("equity") == .orderedSame}
-        } else {
-            return response.data ?? []
-        }
-    }
-    
-    public func fetchQuotes(symbols: String) async throws -> [Quote] {
-        guard var urlComponents = URLComponents(string: "\(baseURL)/v7/finance/quote") else {
-            throw APIError.invalidURL
+        private func urlForChartData(symbol: String, range: ChartRange) -> URL? {
+            guard var urlComp = URLComponents(string: "\(baseURL)/v8/finance/chart/\(symbol)") else {
+                return nil
+            }
+            
+            urlComp.queryItems = [
+                URLQueryItem(name: "range", value: range.rawValue),
+                URLQueryItem(name: "interval", value: range.interval),
+                URLQueryItem(name: "indicators", value: "quote"),
+                URLQueryItem(name: "includeTimestamps", value: "true")
+            ]
+            return urlComp.url
         }
         
-        urlComponents.queryItems = [.init(name: "symbols", value: symbols)]
-        guard let url = urlComponents.url else {
-            throw APIError.invalidURL
+        public func searchTickers(query: String, isEquityTypeOnly: Bool = true) async throws -> [Ticker] {
+            guard let url = urlForSearchTickers(query: query) else { throw APIError.invalidURL }
+            let (resp, statusCode): (SearchTickersResponse, Int) = try await fetch(url: url)
+            if let error = resp.error {
+                throw APIError.httpStatusCodeFailed(statusCode: statusCode, error: error)
+            }
+            let data = resp.data ?? []
+            if isEquityTypeOnly {
+                return data.filter { ($0.quoteType ?? "").localizedCaseInsensitiveCompare("equity") == .orderedSame }
+            } else {
+                return data
+            }
         }
         
-        let (response, statusCode): (QuoteResponse, Int) = try await fetch(url: url)
-        if let error = response.error {
-            throw APIError.httpStatusCodeFailed(statusCode: statusCode, error: error)
-        }
-        return response.data ?? []
-    }
-    
-    private func fetch<D: Decodable>(url: URL) async throws -> (D, Int) {
-        let (data, response) = try await session.data(from: url)
-        let statusCode = try validateHTTPResponse(response)
-        return (try jsonDecoder.decode(D.self, from: data), statusCode)
-    }
-    
-    private func validateHTTPResponse(_ response: URLResponse) throws -> Int {
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw APIError.invalidResponseType
+        public func searchTickersRawData(query: String, isEquityTypeOnly: Bool) async throws -> (Data, URLResponse) {
+            guard let url = urlForSearchTickers(query: query) else { throw APIError.invalidURL }
+            return try await session.data(from: url)
         }
         
-        guard 200...299 ~= httpResponse.statusCode ||
-                400...499 ~= httpResponse.statusCode
-        else {
-            throw APIError.httpStatusCodeFailed(statusCode: httpResponse.statusCode , error: nil)
+        private func urlForSearchTickers(query: String) -> URL? {
+            guard var urlComp = URLComponents(string: "\(baseURL)/v1/finance/search") else {
+                return nil
+            }
+            
+            urlComp.queryItems = [
+                URLQueryItem(name: "lang", value: "en-US"),
+                URLQueryItem(name: "quotesCount", value: "20"),
+                URLQueryItem(name: "q", value: query)
+            ]
+            return urlComp.url
         }
-        return httpResponse.statusCode
-    }
+        
+        public func fetchQuotes(symbols: String) async throws -> [Quote] {
+            guard let url = urlForFetchQuotes(symbols: symbols) else { throw APIError.invalidURL }
+            let (resp, statusCode): (QuoteResponse, Int) = try await fetch(url: url)
+            if let error = resp.error {
+                throw APIError.httpStatusCodeFailed(statusCode: statusCode, error: error)
+            }
+            return resp.data ?? []
+        }
+        
+        public func fetchQuotesRawData(symbols: String) async throws -> (Data, URLResponse) {
+            guard let url = urlForFetchQuotes(symbols: symbols) else { throw APIError.invalidURL }
+            return try await session.data(from: url)
+        }
+        
+        private func urlForFetchQuotes(symbols: String) -> URL? {
+            guard var urlComp = URLComponents(string: "\(baseURL)/v7/finance/quote") else {
+                return nil
+            }
+            urlComp.queryItems = [ URLQueryItem(name: "symbols", value: symbols) ]
+            return urlComp.url
+        }
+        
+        private func fetch<D: Decodable>(url: URL) async throws -> (D, Int) {
+            let (data, response) = try await session.data(from: url)
+            let statusCode = try validateHTTPResponse(response: response)
+            return (try jsonDecoder.decode(D.self, from: data), statusCode)
+        }
+        
+        private func validateHTTPResponse(response: URLResponse) throws -> Int {
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw APIError.invalidResponseType
+            }
+            
+            guard 200...299 ~= httpResponse.statusCode ||
+                  400...499 ~= httpResponse.statusCode
+            else {
+                throw APIError.httpStatusCodeFailed(statusCode: httpResponse.statusCode, error: nil)
+            }
+            
+            return httpResponse.statusCode
+        }
 }
