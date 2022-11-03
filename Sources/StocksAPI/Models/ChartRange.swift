3a1e8b24bfcef1f40c636c2526c09c674ea6193b
Alberto Delle Donne <alberto.delledonne05@gmail.com>
2022-11-03 18:33:48 +0000

    Update ChartRange.swift

--- Sources/StocksAPI/Models/ChartRange.swift
+++ Sources/StocksAPI/Models/ChartRange.swift
@@ -12,12 +12,9 @@ public enum ChartRange: String, CaseIterable {
     case oneWeek = "5d"
     case oneMonth = "1mo"
     case threeMonth = "3mo"
-    case sixMonth = "6mo"
     case ytd
     case oneYear = "1y"
-    case twoYear = "2y"
     case fiveYear = "5y"
-    case tenYear = "10y"
     case max
     
     public var interval: String {
@@ -25,8 +22,8 @@ public enum ChartRange: String, CaseIterable {
         case .oneDay: return "1m"
         case .oneWeek: return "5m"
         case .oneMonth: return "90m"
-        case .threeMonth, .sixMonth, .ytd, .oneYear, .twoYear: return "1d"
-        case .fiveYear, .tenYear: return "1wk"
+        case .threeMonth, .ytd, .oneYear: return "1d"
+        case .fiveYear: return "1wk"
         case .max: return "3mo"
         }
     }
